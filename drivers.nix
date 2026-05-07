0bdb85cbf85d84bc3d116a3186ec3bc2976f529c
Vitalii Seledkov <seledkov.vitaliy@gmail.com>
2026-05-07 22:16:04 +0100

    drivers for laptop configuration

---
+++ drivers.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+
+{
+	hardware.opengl = {
+		enable = true;
+		driSupport = true;
+		driSupport32Bit = true;
+	};
+
+	services.xserver.videoDrivers = ["nvidia"]
+	hardware.nvidia.modesetting.enable = true;
+
+	hardware.nvidia.prime = {
+		offload = {
+			enable = true;
+			enableOffloadCmd = true;
+		};
+
+		intelBusId = "PCI:0:2:0";
+		nvidiaBusId = "PCI:1:0:0";
+	};
+}
