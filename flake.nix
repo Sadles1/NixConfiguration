6dbc68dbf76eeb4375f25e643843d397954b8904
sadles <seledkov.vitaliy@gmail.com>
2026-05-09 10:44:18 +0100

    removed niri from flake

--- flake.nix
+++ flake.nix
@@ -2,14 +2,9 @@
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 		nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-
-		niri = {
-			url = "github:niri-wm/niri";
-			inputs.nixpkgs.follows = "nixpkgs-unstable";
-		};
 	};
 
-	outputs = { self, nixpkgs, ...}: {
+	outputs = { self, nixpkgs, nixpkgs-unstable, ...}: {
 		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
 			modules = [
 				{ nix.settings.experimental-features = ["nix-command" "flakes"]; }
